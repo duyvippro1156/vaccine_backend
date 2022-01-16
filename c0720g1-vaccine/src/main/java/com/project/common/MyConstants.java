bba09fa195c5eee4a71479247defe192a0b94f65
duyvippro1156 <maiduybigbang@gmail.com>
2022-01-16 16:08:46 +0700

    update

---
+++ c0720g1-vaccine/src/main/java/com/project/common/MyConstants.java
@@ -0,0 +1,13 @@
+package com.project.common;
+
+public class MyConstants {
+    // Replace with your email here:
+    public static final String MY_EMAIL = "...@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "...";
+
+    // And receiver!
+    public static final String MAIL_RECEIVER= "";
+
+}
